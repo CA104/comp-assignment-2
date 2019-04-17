708c5da01a2c4a918e036d55ce576324a5c8981c
Carlos <carlos_arellano@hotmail.ca>
2019-04-16 20:25:13 -0400

    responsive addNewTask

--- js/main.js
+++ js/main.js
@@ -7,22 +7,25 @@ const taskList = [
 
 const printAllTasks = () => {
     // Maps each row from the all tasks array to some html
-    return taskList.map(newTask => (newTask.task)).join('');
+    document.getElementById('upcoming').innerHTML = taskList.map(newTask => `<li>${newTask.date} <br> ${newTask.task}</li>`).join('');
 }
 
 const addNewTask = () => {
-    // Get the date
-    let date = document.getElementById('date').value;
-    // Get the task name
-    let tasks = document.getElementById('tasks').value;
+    // // Get the date
+    // let date = document.getElementById('date').value;
+    // // Get the task name
+    // let tasks = document.getElementById('tasks').value;
+   
     // Create an object, push to the local data set (array)
     const newTask = taskList.push({date: '2019-04-17', task: 'Waterframe design'})
+    
     // Store the entire list of tasks to localstorage
     localStorage.setItem('tasks', JSON.stringify(taskList));
 
     // Reprint the list of current tasks
-
+    printAllTasks();
     // Clear out what's there in "new"
     // Re-focus the input so I can add more
 }
-document.getElementById('upcoming').innerHTML = printAllTasks(taskList.newTask);
+
+addNewTask();
