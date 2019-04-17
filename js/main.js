5f7e2cfa97ff60e622b934e0ebb33a6e8fc658bd
Carlos <carlos_arellano@hotmail.ca>
2019-04-17 18:44:48 -0400

    responsive taskList

--- js/main.js
+++ js/main.js
@@ -1,31 +1,56 @@
 // All tasks array
-const taskList = [
+let taskList = [
     { date: '2019-04-25', task: 'Prototype assignment' },
     { date: '2019-04-19', task: 'Mini css framework assignment' },
     { date: '2019-04-27', task: 'Comp assignment'}
 ]
 
 const printAllTasks = () => {
+
     // Maps each row from the all tasks array to some html
     document.getElementById('upcoming').innerHTML = taskList.map(newTask => `<li>${newTask.date} <br> ${newTask.task}</li>`).join('');
 }
 
+const addNewTaskBtn = () => {
+    document.getElementById('newTask').submit();
+}
+
 const addNewTask = () => {
-    // // Get the date
-    // let date = document.getElementById('date').value;
-    // // Get the task name
-    // let tasks = document.getElementById('tasks').value;
+    alert("button pressed!")
+
+
+    // // Get the date from UI
+    let date = document.getElementById('date').value;
+    // // Get the task name from UI
+    let tasks = document.getElementById('tasks').value;
    
+    alert(`date: ${date}, task: ${tasks}`)
+
+    // check if there are things already in local storage
+    var items = localStorage.getItem("tasks")
+    if (items == null || items == undefined) {
+        console.log("there is nothing saved in the task list, moving on with life")
+    }
+    else {
+        // convert whatever was in local storage back into an array so you can work with it
+        taskList = JSON.parse(items)
+    }
+
     // Create an object, push to the local data set (array)
-    const newTask = taskList.push({date: '2019-04-17', task: 'Waterframe design'})
+    taskList.push({ "date":date, "task":tasks});
     
     // Store the entire list of tasks to localstorage
     localStorage.setItem('tasks', JSON.stringify(taskList));
 
     // Reprint the list of current tasks
     printAllTasks();
+    // let html = ""
+    // for (let i = 0; i < taskList.length; i++) {
+    //     html += `<li>${taskList[i].date} <br> ${taskList[i].task}</li>`
+    // }
+    document.getElementById('upcoming').innerHTML = html
     // Clear out what's there in "new"
     // Re-focus the input so I can add more
 }
 
-addNewTask();
+// addNewTask();
