65f8fb8a7a28572de1e6f9c290024a07fbf3f43c
Carlos <carlos_arellano@hotmail.ca>
2019-04-19 14:57:06 -0400

    refocus and clear input box

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
 // All tasks array
-let taskList = [
-    { date: '2019-04-25', task: 'Prototype assignment' },
-    { date: '2019-04-19', task: 'Mini css framework assignment' },
-    { date: '2019-04-27', task: 'Comp assignment'}
-]
+let taskList = []
 
 const printAllTasks = () => {
 
     // Maps each row from the all tasks array to some html
-    document.getElementById('upcoming').innerHTML = taskList.map(newTask => `<li>${newTask.date} <br> ${newTask.task}</li>`).join('');
+    document.getElementById('upcoming').innerHTML = taskList.map(newTask => `<li>${newTask.date} <br> ${newTask.task}</li> 
+        <br> <a href= "https://wddm2019.slack.com/messages/CF8CBLGBW/" target= "_blank">`).join('');
 }
 
 const addNewTask = () => {
@@ -17,13 +14,11 @@ const addNewTask = () => {
     let date = document.getElementById('date').value;
     // // Get the task name from UI
     let tasks = document.getElementById('tasks').value;
-   
-    alert(`date: ${date}, task: ${tasks}`)
 
     // check if there are things already in local storage
     var items = localStorage.getItem("tasks")
     if (items == null || items == undefined) {
-        console.log("there is nothing saved in the task list, moving on with life")
+        console.log("No tasks available")
     }
     else {
         // convert whatever was in local storage back into an array so you can work with it
@@ -39,9 +34,11 @@ const addNewTask = () => {
     // Reprint the list of current tasks
     printAllTasks();
 
-    document.getElementById('upcoming').innerHTML = html
     // Clear out what's there in "new"
+    document.getElementById('tasks').value = " ";
+
     // Re-focus the input so I can add more
+    document.getElementById('tasks').focus();
 }
 
 addNewTask();
