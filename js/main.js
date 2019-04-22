c3233ebf219406bc2385092f1b3661ab6fab7cba
Carlos <carlos_arellano@hotmail.ca>
2019-04-22 19:11:28 -0400

    separeted functions and fixed issue with extra li added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-let taskList = [] // All tasks array
 
 const printAllTasks = () => {
     
@@ -8,31 +7,50 @@ const printAllTasks = () => {
         <br> ${newTask.course}</li>`).join('');
 }
 
+const taskForm = document.getElementById('newTask');
 const addNewTask = () => {
 
+    // prevent the page from refreshing
+    event.preventDefault();
     
     let date = document.getElementById('date').value;  // Get the date from UI
     
     let tasks = document.getElementById('tasks').value;  // Get the task name from UI
     
     let course = document.getElementById('course').value;  // Get the course selection from UI
-
     
+    taskList.push({ "date":date, "task":tasks, "course":course }); // Create an object, push to the local data set (array)
+    
+    taskForm.reset(); // Clear out what's there in "new"
+    setToLocalStorage();
+    printAllTasks();
+}
+taskForm.addEventListener('submit', addNewTask);
+
+
+const getFromLocalStorage = () => {
+
     var items = localStorage.getItem("tasks") // check if there's anything in local storage
     if (items == "" || items == undefined) {
         console.log("No tasks available")
+        return [];
     }
     else {
-        taskList = JSON.parse(items) // convert whatever was in local storage back into an array so you can work with it
+        return JSON.parse(items) // convert whatever was in local storage back into an array so you can work with it
     }
-
-    taskList.push({ "date":date, "task":tasks, "course":course }); // Create an object, push to the local data set (array)
-    
-    localStorage.setItem('tasks', JSON.stringify(taskList)); // Store the entire list of tasks to localstorage
-
-    printAllTasks(); // Reprint the list of current tasks
-
-    document.getElementById('tasks').value = " "; // Clear out what's there in "new"
 }
 
-addNewTask();
+
+const setToLocalStorage = () => {
+
+    localStorage.setItem('tasks', JSON.stringify(taskList)); // Store the entire list of tasks to localstorage
+
+   // printAllTasks(); // Reprint the list of current tasks
+
+}
+
+
+
+let taskList = getFromLocalStorage(); // All tasks array
+
+printAllTasks();
