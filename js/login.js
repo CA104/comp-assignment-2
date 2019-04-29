1517e2ad248b6b347ce2c77772b35c6efbfaefb5
Carlos <carlos_arellano@hotmail.ca>
2019-04-28 23:51:41 -0400

    added commentary

--- js/login.js
+++ js/login.js
@@ -1,15 +1,20 @@
-let students = [
+/********** current users **********/
+let students = [ // array of students with existing logins
     {studentUsername: 'n01062419', studentPassword: 'carlos123'},
     {studentUsername: 'n02847541', studentPassword: 'mazen456'},
     {studentUsername: 'n03750924', studentPassword: 'nicole789'},
     {studentUsername: 'n04872047', studentPassword: 'rocco101'}
 ];
 
-let login = ( studentsLogins ) => {
+/********** login validator **********/
+let login = ( studentsLogins ) => { // fucntion to validate username and password
     students.forEach( studentsLogins => {if (( studentsLogins.studentUsername ===
         document.getElementById('username').value) && (studentsLogins.studentPassword 
-            === document.getElementById('password').value)); {
+            === document.getElementById('password').value)) {
                 window.location.href="index.html"
             }
+            else // NOTE: issue with the alert popping on screen for each element when the username and/or password are incorrect
+            alert('Wrong username or password')
+            return;
     });
 }
